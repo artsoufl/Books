fa11b095adb314f71cf14bc57238650b418bd8c7
EUROPE\artsoufl <Artemis.Tsouflidou@microsoft.com>
2020-06-24 13:33:37 +0100

    automapper

---
+++ BooksAsync.API/Filters/BookResultFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksAsync.API.Filters
+{
+    public class BookResultFilterAttribute : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            var result =  context.Result as ObjectResult;
+            if (result?.Value == null || result.StatusCode < 200
+                || result.StatusCode >= 300)
+            {
+                await next();
+                return;
+            }
+
+            await next();
+            //return base.OnResultExecutionAsync(context, next);
+        }
+    }
+}
--- BooksAsync.API/Startup.cs
+++ BooksAsync.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using BooksAsync.API.Contexts;
 using BooksAsync.API.Services;
 using Microsoft.AspNetCore.Builder;
@@ -34,6 +35,8 @@ namespace BooksAsync.API
             services.AddDbContext<BookContext>(o => o.UseSqlServer(connectioString));
 
             services.AddScoped<IBooksRepository, BooksRepository>();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
