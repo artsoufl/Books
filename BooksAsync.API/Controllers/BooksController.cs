fd36251defb02c4460f6f211511db908d4b4cc8e
EUROPE\artsoufl <Artemis.Tsouflidou@microsoft.com>
2020-06-24 10:00:25 +0100

    books controller

---
+++ BooksAsync.API/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using BooksAsync.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksAsync.API.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksRepository _booksRepository;
+
+        public BooksController(IBooksRepository repo)
+        {
+            _booksRepository = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var bookEntities = await _booksRepository.GetBooksAsync();
+            return Ok(bookEntities);
+        }
+    }
+}
